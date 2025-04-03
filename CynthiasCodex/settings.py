e8424538e97a19e32b749408de56716e39f0a92d
Edson Morro Nogueira <125297431+EdsonMorroNogueira@users.noreply.github.com>
2025-04-02 22:07:52 -0300

    Update settings.py

--- CynthiasCodex/settings.py
+++ CynthiasCodex/settings.py
@@ -93,11 +93,14 @@ DATABASES = {
 }
 
 #S3
-AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+# AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+# AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_BUCKET_NAME')
 AWS_S3_REGION_NAME = os.getenv('AWS_REGION', 'us-east-1')
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+AWS_QUERYSTRING_AUTH = False
+AWS_DEFAULT_ACL = None
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
