d1457fe2f63969e8fece3ab9c98a54dff4ed774c
Edson Morro Nogueira <125297431+EdsonMorroNogueira@users.noreply.github.com>
2025-04-02 22:45:51 -0300

    Update views.py

--- uploads/views.py
+++ uploads/views.py
@@ -9,7 +9,10 @@ import json
 
 # Create your views here.
 
-s3_client = boto3.client('s3')
+s3_client = boto3.client('s3',
+                          aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+                          aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+                          region_name=settings.AWS_S3_REGION_NAME)
 
 
 def index(request):
